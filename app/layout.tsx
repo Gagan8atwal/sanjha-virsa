718dfd92cfdb74df10dc799386baf3656a86737a
Gagan8atwal <gagan.s.atwal@gmail.com>
2026-07-09 16:28:19 -0700

    Add navigation layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import SideMenu from '../components/SideMenu';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SideMenu />
+        <div className="lg:pl-64">{children}</div>
+      </body>
     </html>
   );
 }
